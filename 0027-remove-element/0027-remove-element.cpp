8bdd36839ad30dc003b511977bed587f8fe66e5b
Ashu Sharma <71217732+ashusharma-cs@users.noreply.github.com>
2026-03-10 16:46:31 -0700

    Time: 0 ms (100%), Space: 11.6 MB (83.26%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+
+        int slow=0, fast=0;
+
+        for(fast;fast<nums.size();fast++)
+        {
+            if(nums[fast]!=val)
+            {
+                nums[slow]=nums[fast];
+                slow++;
+            }
+        }
+
+        return slow;
+        
+    }
+};
