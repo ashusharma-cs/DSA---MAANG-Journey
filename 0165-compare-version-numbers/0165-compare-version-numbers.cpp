12d6c2a1b06856f0170a63d1ecdbc905f67db5b2
Ashu Sharma <71217732+ashusharma-cs@users.noreply.github.com>
2026-03-13 16:27:42 -0700

    Time: 0 ms (100%), Space: 8.1 MB (82.56%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -7,36 +7,25 @@ public:
 
         while(p1<version1.length() || p2<version2.length())
         {
-            string num1="";
-            string num2="";
+            int num1=0;
+            int num2=0;
 
             while(p1<version1.length() && version1[p1]!='.')
             {
-                num1+=version1[p1];
+                num1=num1*10+(version1[p1]-'0');
                 p1++;
             }
 
 
             while(p2<version2.length() && version2[p2]!='.')
             {
-                num2+=version2[p2];
+                num2=num2*10+(version2[p2]-'0');
                 p2++;
             }
 
-            if(num1.empty())
-            {
-                num1='0';
-            }
-
-            if(num2.empty())
-            {
-                num2='0';
-            }
-            int n1=stoi(num1);
-            int n2=stoi(num2);
-
-            if(n1<n2) return res=-1;
-            else if(n1>n2) return res=1;
+           
+            if(num1<num2) return res=-1;
+            else if(num1>num2) return res=1;
 
             p1++;
             p2++;
