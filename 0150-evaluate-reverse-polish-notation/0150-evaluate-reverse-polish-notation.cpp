d826ffbc97dffd1328fb13e915a33341d3d972a5
Ashu Sharma <71217732+ashusharma-cs@users.noreply.github.com>
2026-04-25 16:32:14 -0700

    Time: 0 ms (100%), Space: 17.1 MB (65.93%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -2,53 +2,98 @@ class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
 
-        int result;
-        stack<int> st;
+        // int result;
+        // stack<int> st;
 
-        for(auto const &token:tokens)
+    //     for(auto const &token:tokens)
+    //     {
+    //         if(token=="+" || token=="-" || token=="*" || token=="/")
+    //         {
+    //            if(st.size()>=2)
+    //             {
+    //                 int rightOperand=st.top();
+    //                 st.pop();
+
+    //                 int leftOperand=st.top();
+    //                 st.pop();
+    //                 if(token=="+")
+    //                 {
+    //                     int sum=leftOperand+rightOperand;
+    //                     st.push(sum);
+    //                 }
+    //                 else if(token=="-")
+    //                 {
+    //                     int diff=leftOperand-rightOperand;
+    //                     st.push(diff);
+    //                 }
+    //                 else if(token=="*")
+    //                 {
+    //                     int prod=leftOperand*rightOperand;
+    //                     st.push(prod);
+    //                 }
+    //                 else
+    //                 {
+    //                     int div=leftOperand/rightOperand;
+    //                     st.push(div);
+    //                 }
+    //             }
+    //         }
+    //         else
+    //         {
+    //             st.push(stoi(token));
+    //         }
+    //     }
+        
+    //     if(!st.empty())
+    //     {
+    //         result=st.top();
+    //     }
+
+    //     return result;
+    // }
+
+    stack<int> st;
+    
+    for(auto const &token:tokens)
+    {
+        if(token!="+"&&token!="-" && token!="*" && token!="/")
         {
-            if(token=="+" || token=="-" || token=="*" || token=="/")
+            st.push(stoi(token));
+        }
+        else
+        {
+            if(!st.empty())
             {
-               if(st.size()>=2)
+                int num2=st.top();
+                st.pop();
+                int num1=st.top();
+                st.pop();
+                
+                
+                if(token=="+")
                 {
-                    int rightOperand=st.top();
-                    st.pop();
-
-                    int leftOperand=st.top();
-                    st.pop();
-                    if(token=="+")
-                    {
-                        int sum=leftOperand+rightOperand;
-                        st.push(sum);
-                    }
-                    else if(token=="-")
-                    {
-                        int diff=leftOperand-rightOperand;
-                        st.push(diff);
-                    }
-                    else if(token=="*")
-                    {
-                        int prod=leftOperand*rightOperand;
-                        st.push(prod);
-                    }
-                    else
-                    {
-                        int div=leftOperand/rightOperand;
-                        st.push(div);
-                    }
+                    int sum=num1+num2;
+                    st.push(sum);
+                }
+                else if(token=="-")
+                {
+                    int diff=num1-num2;
+                    st.push(diff);
+                }
+                else if(token=="*")
+                {
+                    int prod=num1*num2;
+                    st.push(prod);
+                }
+                else
+                {
+                    int quotient=num1/num2;
+                    st.push(quotient);
                 }
             }
-            else
-            {
-                st.push(stoi(token));
-            }
         }
-        
-        if(!st.empty())
-        {
-            result=st.top();
-        }
-
-        return result;
+    }
+    
+    return st.top();
     }
 };
